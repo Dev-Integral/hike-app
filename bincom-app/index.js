aa27d0719982891d867151b0c8b417d3556f955d
Ayoola <tayoola13@yahoo.com>
2021-01-28 08:25:46 +0100

    Rendered with pug pug

--- bincom-app/index.js
+++ bincom-app/index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get('/delta', (req, res) => {
-    res.sendFile(__dirname + './pages/delta.html');
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+router.get('/delta', (req, res) => {
+    res.render("delta", { title: "Hey", message: "Hello there!" });
 });
-app.get('/polls', (req, res) => {
-    res.sendFile(__dirname + './pages/polls.html');
+router.get('/polls', (req, res) => {
+    res.sendFile(__dirname + '/pages/polls.html');
 });
 
+// add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log('Running at Port 3000');

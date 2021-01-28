f74ce47c96ed409a23a3a7d0385435c300e71431
Ayoola <tayoola13@yahoo.com>
2021-01-28 18:52:35 +0100

    fetched the data

--- bincom-app/index.js
+++ bincom-app/index.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const https = require('https');
+
+let deltaPolls = '';
+let request = https.get('https://drive.google.com/file/d/0B77xAtHK1hd4Ukx6SHpqTkd6TWM/view', (res) => {
+    if (res.statusCode !== 200) {
+        console.error(`Couldn't get data`);
+        res.resume();
+        return;
+    } else {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+        res.on('close', () => {
+            console.log('Retrieved all data');
+            deltaPolls = (JSON.parse(data));
+        });
+        request.on('error', (err) => {
+            console.error(`Encountered an error trying to make request:${err.message}`)
+        })
+    }
+});
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
